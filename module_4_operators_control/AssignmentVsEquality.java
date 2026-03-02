85e2580890888cd4ca9c86758b400273cc0c9b61
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:34:05 +0530

    Showing Assignment and Equality operators in a single examples

--- module_4_operators_control/AssignmentVsEquality.java
+++ module_4_operators_control/AssignmentVsEquality.java
@@ -1,4 +1,12 @@
 package module_4_operators_control;
 
 public class AssignmentVsEquality {
+    public static void main(String[] args) {
+
+        int a = 5; // assignment
+
+        if (a == 5) { // comparison
+            System.out.println("a equals 5");
+        }
+    }
 }
