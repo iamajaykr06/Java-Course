ee712852c8fafd5873b5d4279c3636254aace99b
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:42:16 +0530

    Program for checking Whether a number is positive, negative or Zero

--- module_4_operators_control/PositiveNegativeZero.java
+++ module_4_operators_control/PositiveNegativeZero.java
@@ -1,4 +1,22 @@
 package module_4_operators_control;
 
+import java.util.Scanner;
+
 public class PositiveNegativeZero {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+
+        sc.close();
+    }
 }
