7dc2e2e2401dd841acca7f29da824d64ead26122
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:52:54 +0530

    Learn And Implement Logical Operators

--- module_4_operators_control/LogicalOperatorsDemo.java
+++ module_4_operators_control/LogicalOperatorsDemo.java
@@ -1,4 +1,22 @@
 package module_4_operators_control;
 
 public class LogicalOperatorsDemo {
+    public static void main(String[] args) {
+
+        int age = 25;
+
+        if (age > 18 && age < 60) {
+            System.out.println("Working age");
+        }
+
+        if (age < 18 || age > 60) {
+            System.out.println("Not working age");
+        }
+
+        boolean isLoggedIn = false;
+
+        if (!isLoggedIn) {
+            System.out.println("Please login");
+        }
+    }
 }
