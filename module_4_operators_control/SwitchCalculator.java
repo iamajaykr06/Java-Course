a327bb42b8aab576040076a0288169fd2b0c694e
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:47:52 +0530

    Made a Simple Calculator Using SWITCH Statement

--- module_4_operators_control/SwitchCalculator.java
+++ module_4_operators_control/SwitchCalculator.java
@@ -1,4 +1,41 @@
 package module_4_operators_control;
 
+import java.util.Scanner;
+
 public class SwitchCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                if (b != 0)
+                    System.out.println(a / b);
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+
+        sc.close();
+    }
 }
