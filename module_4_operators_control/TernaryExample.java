35b23ddcbb8d428653cca8bdeac162d7ce3cee25
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:50:11 +0530

    Rewritting code of ODD-EVEN Using Ternary Operators which enhances readability and decreases Number Of code lines

--- module_4_operators_control/TernaryExample.java
+++ module_4_operators_control/TernaryExample.java
@@ -1,4 +1,12 @@
 package module_4_operators_control;
 
 public class TernaryExample {
+    public static void main(String[] args) {
+
+        int num = 7;
+
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(result);
+    }
 }
