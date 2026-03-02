c043953bd091cf4ea37dbf992f730a3e0506e139
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:36:29 +0530

    Adding example of Basics IF-ELSE Statements

--- module_4_operators_control/IfElseBasics.java
+++ module_4_operators_control/IfElseBasics.java
@@ -1,4 +1,14 @@
 package module_4_operators_control;
 
 public class IfElseBasics {
+    public static void main(String[] args) {
+
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Not Positive");
+        }
+    }
 }
