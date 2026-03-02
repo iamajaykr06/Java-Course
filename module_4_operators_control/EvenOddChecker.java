d0d391f45e596842f2cb06319bf7cd72f46cfaea
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-02 21:38:46 +0530

    Solving Even-Oddd problem

--- module_4_operators_control/EvenOddChecker.java
+++ module_4_operators_control/EvenOddChecker.java
@@ -1,4 +1,21 @@
 package module_4_operators_control;
 
+import java.util.Scanner;
+
 public class EvenOddChecker {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        sc.close();
+    }
 }
