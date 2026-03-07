c43126b39c2409727faabe664d4ea833875d7ac9
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-07 20:48:55 +0530

    I solve the problem of forming new array from given two arrays

--- Module_6_Arrays/CommonElements.java
+++ Module_6_Arrays/CommonElements.java
@@ -1,4 +1,37 @@
 package Module_6_Arrays;
 
+import java.util.Scanner;
+
 public class CommonElements {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[m];
+
+        for(int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        for(int i = 0; i < m; i++)
+            b[i] = sc.nextInt();
+
+        int[] common = new int[Math.min(n, m)];
+        int k = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(a[i] == b[j]) {
+                    common[k++] = a[i];
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i < k; i++)
+            System.out.print(common[i] + " ");
+    }
 }
