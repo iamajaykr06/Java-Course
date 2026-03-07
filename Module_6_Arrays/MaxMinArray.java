07a72b68dff72ed768ca1dfee54e7ce5e1fd4aad
Ajay Kumar <iamajayk.r06@gmail.com>
2026-03-07 21:02:51 +0530

    solving problem of finding Minimum and Maximum Value In an array

--- Module_6_Arrays/MaxMinArray.java
+++ Module_6_Arrays/MaxMinArray.java
@@ -1,4 +1,31 @@
 package Module_6_Arrays;
 
+import java.util.Scanner;
+
 public class MaxMinArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i = 1; i < n; i++) {
+
+            if(arr[i] > max)
+                max = arr[i];
+
+            if(arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+    }
 }
